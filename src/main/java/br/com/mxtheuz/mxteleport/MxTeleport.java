c60de9bfe1b510cc4dce791544d7d542b2e8547d
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-02 21:41:24 -0300

    Teleport plugin: finish version

---
+++ src/main/java/br/com/mxtheuz/mxteleport/MxTeleport.java
@@ -0,0 +1,51 @@
+package br.com.mxtheuz.mxteleport;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MxTeleport extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("[mx-teleport] Teleporte esta funcionando!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("teleport")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("Este comando so pode ser usado por jogadores.");
+                return true;
+            }
+
+            Player player = (Player) sender;
+
+            if (args.length < 1) {
+                player.sendMessage("Uso: /teleport <localizacao>");
+                return true;
+            }
+
+            String locationName = args[0];
+            World targetLocation = Bukkit.getWorld(locationName); // Substitua "house" pelo nome do seu mundo
+
+            if (targetLocation == null) {
+                player.sendMessage("O mundo de destino nao foi encontrado.");
+                return true;
+            }
+
+            player.teleport(targetLocation.getSpawnLocation());
+            player.sendMessage("Voce foi teleportado para " + locationName + ".");
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
